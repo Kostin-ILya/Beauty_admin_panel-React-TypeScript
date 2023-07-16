1cb389405bc3816ccb8bcd27f84ce9a3b38fc208
Ilya <lord-ilja@yandex.ru>
2023-07-16 16:46:24 +0300

    add  routing (v6) + links header

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,22 +1,46 @@
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+
 import Header from '../header/Header'
 import SchedulePage from '../../pages/schedule/SchedulePage'
 
-// import HistoryPage from "../../pages/history/HistoryPage";
-// import CancelModal from "../modal/CancelModal";
+import HistoryPage from '../../pages/history/HistoryPage'
 
 import { AppointmentContextProvider } from '../../context/appointments/AppointmentContext'
 import './app.scss'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <SchedulePage />,
+      },
+      {
+        path: 'schedule',
+        element: <SchedulePage />,
+      },
+      {
+        path: 'history',
+        element: <HistoryPage />,
+      },
+    ],
+  },
+])
+
 function App() {
+  return <RouterProvider router={router} />
+}
+
+function Root() {
   return (
-    <AppointmentContextProvider>
-      <main className="board">
-        <Header />
-        <SchedulePage />
-        {/* <HistoryPage /> */}
-        {/* <CancelModal /> */}
-      </main>
-    </AppointmentContextProvider>
+    <main className="board">
+      <Header />
+      <AppointmentContextProvider>
+        <Outlet />
+      </AppointmentContextProvider>
+    </main>
   )
 }
 
